943e640a49675ee633917ed8be34a01b748474cf
wcotton229 <32826633+wcotton229@users.noreply.github.com>
2018-04-04 23:22:11 -0500

    Cleaned up code and EVERYTHING IS WORKINGgit add -A!

--- liri.js
+++ liri.js
@@ -1,131 +1,152 @@
-//Grab data from keys.js
-var keys = require('./keys.js');
-var request = require('request');
-var twitter = require('twitter');
-var spotify = require('spotify');
-var client = new twitter(keys.twitterKeys);
+require("dotenv").config();
+
+var keys = require("./keys.js");
 var fs = require('fs');
+var request = require('request');
 
-//Stored argument's array
-var nodeArgv = process.argv;
-var command = process.argv[2];
-//movie or song
-var x = "";
-//attaches multiple word arguments
-for (var i = 3; i < nodeArgv.length; i++) {
-    if (i > 3 && i < nodeArgv.length) {
-        x = x + "+" + nodeArgv[i];
-    } else {
-        x = x + nodeArgv[i];
+//Spotify require
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+
+
+//Twitter require
+var Twitter = require('twitter');
+var client = new Twitter(keys.twitter);
+
+
+//command line arguments
+var args = process.argv;
+var command = args[2];
+
+var input = "";
+for (var i = 3; i < args.length; i++) {
+    input += args[i] + " ";
+}
+
+function processInput() {
+    switch (command) {
+        case 'my-tweets':
+            tweet();
+            break;
+        case 'spotify-this-song':
+            songInfo(input);
+            break;
+        case 'movie-this':
+            movie(input);
+            break;
+        case 'do-what-it-says':
+            randomInfo();
+            break;
+        default:
+            console.log("Please enter a valid command. The choices are: \n my-tweets\n spotify-this-song\n movie-this\n do-what-it-says");
+            break;
     }
 }
 
-//switch case
-switch (command) {
-    case "my-tweets":
-        showTweets();
-        break;
 
-    case "spotify-this-song":
-        if (x) {
-            spotifySong(x);
-        } else {
-            spotifySong("Fluorescent Adolescent");
+// my-tweets
+
+function tweet() {
+    console.log("show last 20 tweets");
+    var params = { screen_name: 'Billywaynec1', count: 20 };
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            var output = "MY TWEETS: \n";
+            tweets.forEach(element => {
+                output += element.text + '\n' + element.created_at + '\n\n';
+            }
+            );
+            console.log(output);
         }
-        break;
-
-    case "movie-this":
-        if (x) {
-            omdbData(x)
-        } else {
-            omdbData("Mr. Nobody")
-        }
-        break;
-
-    case "do-what-it-says":
-        doThing();
-        break;
-
-    default:
-        console.log("{Please enter a command: my-tweets, spotify-this-song, movie-this, do-what-it-says}");
-        break;
+        else
+            return error;
+    });
 }
 
-function showTweets() {
-    //Display last 20 Tweets
-    function tweet() {
-        console.log("show last 20 tweets");
-        var params = { screen_name: 'Billywaynec1', count: 20 };
-        client.get('statuses/user_timeline', params, function (error, tweets, response) {
-            if (!error) {
-                var output = "MY TWEETS: \n";
-                tweets.forEach(element => {
-                    output += element.text + '\n' + element.created_at + '\n\n';
-                }
-                );
-                console.log(output);
-                logThis(output);
+
+// spotify-this-song
+
+function songInfo(songName) {
+
+    if (songName === "") {
+        songName = "Numb";
+    }
+    spotify.search({ type: 'track', query: songName, limit: 5 }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        var searchedSong = "SPOTIFY THIS SONG: " + songName + "\n";
+        data.tracks.items.forEach(element => {
+            //console.log(element);
+            var artistNames = "";
+
+            for (var i = 0; i < element.artists.length; i++) {
+                if (i !== 0) artistNames += ", ";
+                artistNames += element.artists[i].name;
             }
-            else
-                return error;
+
+            searchedSong += "Artist(s): " + artistNames + '\n';
+
+            searchedSong += "Song Title: " + element.name + '\n';
+
+            searchedSong += "Preview Link: " + element.preview_url + '\n';
+
+            searchedSong += "Album: " + element.album.name + '\n\n';
+
         });
+
+        console.log(searchedSong);
+    });
+}
+
+
+//  movie-this
+
+function movie(movieName) {
+
+    if (movieName === "") {
+        movieName = "Mr.Nobody";
+        return;
     }
 
-    function spotifySong(song) {
-        spotify.search({ type: 'track', query: song }, function (error, data) {
-            if (!error) {
-                for (var i = 0; i < data.tracks.items.length; i++) {
-                    var songData = data.tracks.items[i];
-                    //artist
-                    console.log("Artist: " + songData.artists[0].name);
-                    //song name
-                    console.log("Song: " + songData.name);
-                    //spotify preview link
-                    console.log("Preview URL: " + songData.preview_url);
-                    //album name
-                    console.log("Album: " + songData.album.name);
-                    console.log("-----------------------");
-                }
-            } else {
-                console.log('Error occurred.');
-            }
-        });
-    }
+    // OMDB API
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
-    function omdbData(movie) {
-        var omdbURL = 'http://www.omdbapi.com/?t=' + movie + '&plot=short&tomatoes=true';
+    request(queryUrl, function (error, response, body) {
 
-        request(omdbURL, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                var body = JSON.parse(body);
+        if (!error && response.statusCode === 200) {
+            var movie = JSON.parse(body);
+            // console.log(movie);
+            console.log("Title: " + movie.Title);
+            console.log("Release Year: " + movie.Year);
+            console.log("IMdB Rating: " + movie.imdbRating);
+            console.log("Country: " + movie.Country);
+            console.log("Language: " + movie.Language);
+            console.log("Plot: " + movie.Plot);
+            console.log("Actors: " + movie.Actors);
+            console.log("Rotten Tomatoes Rating: " + movie.Ratings[1].Value);
 
-                console.log("Title: " + body.Title);
-                console.log("Release Year: " + body.Year);
-                console.log("IMdB Rating: " + body.imdbRating);
-                console.log("Country: " + body.Country);
-                console.log("Language: " + body.Language);
-                console.log("Plot: " + body.Plot);
-                console.log("Actors: " + body.Actors);
-                console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
-                console.log("Rotten Tomatoes URL: " + body.tomatoURL);
+        }
+        else {
+            console.log(response);
+        }
+    });
+}
 
-            } else {
-                console.log('Error occurred.')
-            }
-            if (movie === "Inception") {
-                console.log("-----------------------");
-                console.log("If you haven't watched 'Inception,' then you should: http://www.imdb.com/title/tt0485947/");
 
-            }
-        });
+//  do-what-it-says
 
-    }
+function randomInfo() {
+    fs.readFile("./random.txt", "utf8", function (error, data) {
+        console.log("Processing do what it says...");
+        if (error) return error;
 
-    function doThing() {
-        fs.readFile('random.txt', "utf8", function (error, data) {
-            var txt = data.split(',');
+        //console.log(data);
+        var dataArr = data.split(",");
+        command = dataArr[0];
+        input = dataArr[1];
+        processInput();
+    });
+}
 
-            spotifySong(txt[1]);
-        });
-    };
-};
+processInput();
